b9ca322945e76805280966b5bb3d9ed1b4301537
martyminion <martkimwaweru@gmail.com>
2020-04-25 14:24:42 +0300

    tests use of base html, adds index.html display

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,14 @@
 from flask import render_template
 from . import main
+from ..requests import get_sources
 
 @main.route("/")
 def index():
   '''
   returns the index page and its data
   '''
+  #Getting the sources
+  sources = get_sources()
+  title = "Home - News App"
 
-  return render_template('index.html')
+  return render_template('index.html',sources = sources, title = title)
